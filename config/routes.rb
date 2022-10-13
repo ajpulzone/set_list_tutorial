ff2480f451f0e326b20211834dac8bf92386b845
Annie Pulzone <ajpulzone@gmail.com>
2022-10-12 21:18:36 -0700

    Adds artist_songs_index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/songs", to: "songs#index"
+  get "/songs/:id", to: "songs#show"
+  get "/artists/:artist_id/songs", to: "artist_songs#index"
+
+
+  #get "/show/:me/the/songs", to: "songs#display_them"
+  #above would match localhost:3000/show/jamison/the/songs
+  #above would match localhost:3000/show/202523095720/the/songs
+  #:me is a dynamic segment, can be anything contained within that segment, called dynamic routing
+  
 end
